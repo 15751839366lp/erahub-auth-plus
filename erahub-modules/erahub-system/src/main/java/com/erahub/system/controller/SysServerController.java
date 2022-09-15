920246f70e1f1b301c17a6770b51a0949bfd05a8
李彭 <15751839366@163.com>
2022-09-15 19:19:24 +0800

    新增服务监控功能

---
+++ erahub-modules/erahub-system/src/main/java/com/erahub/system/controller/SysServerController.java
@@ -0,0 +1,28 @@
+package com.erahub.system.controller;
+
+import cn.dev33.satoken.annotation.SaCheckPermission;
+import com.erahub.common.core.server.domain.AjaxResult;
+import com.erahub.common.core.server.domain.Server;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * 服务器监控
+ *
+ * @author ruoyi
+ */
+@RestController
+@RequestMapping("/server")
+public class SysServerController
+{
+    @SaCheckPermission("monitor:server:list")
+    @GetMapping()
+    public AjaxResult getInfo() throws Exception
+    {
+        Server server = new Server();
+        server.copyTo();
+        return AjaxResult.success(server);
+    }
+}
